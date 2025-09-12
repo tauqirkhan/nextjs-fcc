d94faa3384f9c6b6985f0b4f074f994364e4bf3d
tauqirkhan <tauqirkhann.11@gmail.com>
2025-09-12 17:28:49 +0530

    feat: add logout route

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        const response = NextResponse.json(
+            {
+                message: "Log Out Successful",
+                success: true
+            }
+        )
+        response.cookies.set("token", "", {httpOnly: true, expires: new Date(0)})
+        return response
+    } catch (error: unknown) {
+        let errorMessage = "Something Went Wrong"
+
+        if(error instanceof Error)
+            errorMessage = error.message
+        
+        return NextResponse.json({error: errorMessage}, {status: 500})
+    }
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,39 @@
+"use client";
+import axios from "axios";
+import Link from "next/navigation";
+import toast, {Toaster} from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
+
 export default function ProfilePage(){
-   return(
+    const router = useRouter()
+
+    const logout = async () => {
+        try {
+            await axios.get("/api/users/logout")
+            toast.success("Log Out Successful")
+            router.push("/login")
+        } catch (error: unknown) {
+            let message = "Something went wrong"
+        
+            if(error instanceof Error)
+                message = error.message
+
+            console.log(message)
+            toast.error(message)
+        }
+    }
+   
+    return(
     <div>
+        <Toaster />
         <h1>Profile</h1>
         <hr />
         <p>Profile Page</p>
+    <hr />
+    <button onClick={logout}>
+        Log Out
+    </button>
     </div>
    ) 
 }
